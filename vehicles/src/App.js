5f52c2d93f0e4a449448b9a7fb3d75a5e4287ba0
Job koech <jobkoech610@gmail.com>
2023-05-04 15:14:55 +0300

    search area

--- vehicles/src/App.js
+++ vehicles/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import Blog from "./Blog";
 import Home from "./Home";
 import NewBlog from "./NewBlog";
@@ -6,16 +6,24 @@ import NewBlog from "./NewBlog";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [isDarkMode, setIsDarkMode] = useState(".light");
+  const [isDarkMode, setIsDarkMode] = useState('light');
+ 
+  const themeClass = isDarkMode ? "light" : "dark";
+
   const toggleTheme = () => {
     setIsDarkMode(!isDarkMode);
   };
-  const themeClass = isDarkMode ? "light" : "dark";
+
+useEffect(() => {
+    document.body.className = themeClass;
+     }, [themeClass]);
   return (
     <div className={`App ${themeClass}`}>
+      <header>
       <button onClick={toggleTheme}>
         {isDarkMode ? "Dark mode" : "Light mode"}
       </button>
+      </header>
 
       <Router>
         <Routes>
@@ -30,3 +38,26 @@ function App() {
 }
 
 export default App;
+
+// import React, { useState, useEffect } from 'react';
+// import './darkMode.css';
+// function App() {
+// const [theme, setTheme] = useState('light');
+// const toggleTheme = () => {
+// if (theme === 'light') {
+// setTheme('dark');
+// } else {
+// setTheme('light');
+// }
+// };
+// useEffect(() => {
+// document.body.className = theme;
+// }, [theme]);
+// return (
+//     <div className={`App ${theme}`}>
+//       <button onClick={toggleTheme}>Toggle Theme</button>
+//       <h1>Hello, world!</h1>
+//     </div>
+//   );
+// }
+// export default App;
