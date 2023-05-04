9102d003233b96519ea4314f582a0d8f37f0f5ec
StevenMo <stevenmolvin@gmail.com>
2023-05-04 15:55:42 +0300

    feat: added search area in blog.js

--- vehicles/src/Blog.js
+++ vehicles/src/Blog.js
@@ -1,39 +1,52 @@
-
 import { useState, useEffect } from "react";
 import NavBar from "./NavBar";
 
 const Blog = () => {
+  const [blog, setBlog] = useState([]);
+  console.log(blog);
 
-    const[blog, setBlog]=useState([])
-    console.log(blog);
+  useEffect(() => {
+    fetch("http://localhost:3000/vehicles")
+      .then((res) => res.json())
+      .then((blogs) => {
+        setBlog(blogs);
+      });
+  }, []);
 
-    useEffect(()=>{
-        fetch('http://localhost:3000/vehicles')
-        .then(res=>res.json())
-        .then(blogs=>{
-            setBlog(blogs)
-        })
-    },[])
+  const filterNames = (e) => {
+    const search = e.target.value.toLowerCase();
+    if (search === "") {
+      fetch("http://localhost:3000/vehicles")
+        .then((res) => res.json())
+        .then((blogs) => {
+          setBlog(blogs);
+        });
+    } else {
+      const filteredNames = blog.filter((blog) =>
+        blog.Make.toLowerCase().includes(search)
+      );
+      setBlog(filteredNames);
+    }
+  };
 
-//   const blogItems =  blog.map((b)=> b.Model )
-    
-    return (  
-        <div>
-            <NavBar />
-<input type="search" name="search" placeholder="search-blog"/>
-              {blog.map((b)=>(
-                
-                <div className="blog-preview" key={b.id}>
-                    <h2>{b.Model}</h2>
-                    <h2>{b.Make}</h2>
-                    <p>{b.Info}</p>
-                    <img src={b.Image} alt="car"/>
-                
-                </div>
-
-            ))}
+  return (
+    <div>
+      <NavBar />
+      <input
+        type="search"
+        onChange={(e) => filterNames(e)}
+        placeholder="search-blog"
+      />
+      {blog.map((b) => (
+        <div className="blog-preview" key={b.id}>
+          <h2>{b.Model}</h2>
+          <h2>{b.Make}</h2>
+          <p>{b.Info}</p>
+          <img src={b.Image} alt="car" />
         </div>
-    );
-}
- 
+      ))}
+    </div>
+  );
+};
+
 export default Blog;
