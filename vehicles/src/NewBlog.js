5b12cb0499e305a63116575ef9df0f504b4b748f
Job koech <jobkoech610@gmail.com>
2023-05-05 02:12:22 +0300

    added confirm alert in form

--- vehicles/src/NewBlog.js
+++ vehicles/src/NewBlog.js
@@ -1,67 +1,80 @@
 import { useState } from "react";
 
 import NavBar from "./NavBar";
-import './New.css';
-
+import "./New.css";
 
 const NewBlog = () => {
+  const [Model, setModel] = useState("");
+  const [Make, setMake] = useState("");
+  const [Info, setInfo] = useState("");
+  const [Image, setImage] = useState("");
 
-    const [Model, setModel] = useState('');
-    const [Make, setMake] = useState('');
-    const [Info, setInfo] = useState('');
-    const [Image,  setImage] = useState('');
+  //  function handleChange(event) {
+  //     setBlog({
+  //       ...blog,
+  //       [event.target.name]: event.target.defaultValue,
+  //     });
+  //     console.log(event.target)
+  //   }
 
-//  function handleChange(event) {
-//     setBlog({
-//       ...blog,
-//       [event.target.name]: event.target.defaultValue,
-//     });
-//     console.log(event.target)
-//   }
-
-const handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     const blog = { Model, Make, Info, Image };
 
     if (window.confirm("Are you sure you want to add this blog?")) {
-    fetch('http://localhost:3000/vehicles', {
-      method: 'POST',
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(blog)
-    }).then(() => {
-      console.log('new blog added');
-    })
-   
-  }
-}
+      fetch("http://localhost:3000/vehicles", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(blog),
+      }).then(() => {
+        console.log("new blog added");
+      });
+    }
+  };
 
- 
-    return ( 
+  return (
+    <div className="create">
+      <NavBar />
 
-        <div className="create">
-            <NavBar />
-            
-            <form onSubmit={handleSubmit}>
-            <h3 id="logo">New Blog</h3>
+      <form onSubmit={handleSubmit}>
+        <h3 id="logo">New Blog</h3>
 
-<label>Model:</label>
-<input type="text"  Value={Model} onChange={(e) => setModel(e.target.value)} required/>
+        <label>Model:</label>
+        <input
+          type="text"
+          Value={Model}
+          onChange={(e) => setModel(e.target.value)}
+          required
+        />
 
- <label>Make:</label>
-<input type="text" Value={Make} onChange={(e) => setMake(e.target.value)} required/>
- 
- <label>Image URL:</label>
- <input type="text" Value={Image} onChange={(e) => setImage(e.target.value)} required/>
- 
- <label>Specifications:</label>
-<input type="text" Value={Info} onChange={(e) => setInfo(e.target.value)} required/>
+        <label>Make:</label>
+        <input
+          type="text"
+          Value={Make}
+          onChange={(e) => setMake(e.target.value)}
+          required
+        />
 
- 
- <input type="submit" name="submit" value="Add-Blog" />
+        <label>Image URL:</label>
+        <input
+          type="text"
+          Value={Image}
+          onChange={(e) => setImage(e.target.value)}
+          required
+        />
 
-            </form>
-        </div>
-     );
-}
- 
+        <label>Specifications:</label>
+        <input
+          type="text"
+          Value={Info}
+          onChange={(e) => setInfo(e.target.value)}
+          required
+        />
+
+        <input type="submit" name="submit" value="Add-Blog" />
+      </form>
+    </div>
+  );
+};
+
 export default NewBlog;
